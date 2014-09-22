c83f7d0657ae66a60e9145605e553c216c1fd68d
Marek Lewandowski <m.lewandowski@cksource.com>
2014-09-22 16:01:52 +0200

    Introduced ViewerFocusManager class.

---
+++ tests/plugins/a11ychecker/ui/ViewerFocusManager.js
@@ -0,0 +1,153 @@
+/* bender-tags: a11ychecker,unit */
+/* bender-include: %TEST_DIR%../_helpers/require.js, %TEST_DIR%../_helpers/requireConfig.js */
+
+( function() {
+	'use strict';
+
+	require( [ 'ui/ViewerFocusManager', 'helpers/sinon/sinon_amd.min' ], function( ViewerFocusManager, sinon ) {
+
+		bender.test( {
+			setUp: function() {
+				this.mgr = new ViewerFocusManager();
+			},
+
+			'test ViewerFocusManager.count': function() {
+				this.mgr.list = [ 1, 2 ];
+				assert.areSame( 2, this.mgr.count(), 'Valid elements count returned' );
+			},
+
+			'test ViewerFocusManager.addItem': function() {
+				var newElem = new CKEDITOR.dom.element( 'div' );
+				this.mgr.addItem( newElem );
+				assert.areSame( 1, this.mgr.list.length, 'List size increased' );
+				assert.areSame( newElem, this.mgr.list[ 0 ], 'Valid value has been inserted' );
+			},
+
+			'test ViewerFocusManager.addItem adding key listener': function() {
+				// addItem() should add a keydown listener to the element.
+				var newElem = new CKEDITOR.dom.element( 'div' );
+				newElem.on = sinon.spy();
+
+				this.mgr.addItem( newElem );
+
+				assert.areSame( 1, newElem.on.callCount, 'CKEDITOR.dom.element.on calls count' );
+				assert.areSame( 'keydown', newElem.on.args[ 0 ][ 0 ], 'Added listener type' );
+			},
+
+			'test ViewerFocusManager.removeItem': function() {
+				var list = this.mgr.list = [ new CKEDITOR.dom.element( 'div' ), new CKEDITOR.dom.element( 'span' ) ];
+				this.mgr.removeItem( 1 );
+
+				assert.areSame( 1, this.mgr.list.length, 'Not all items were removed' );
+				assert.areSame( list[ 0 ], this.mgr.list[ 0 ], 'Correct element was removed' );
+			},
+
+			'test ViewerFocusManager.removeItem by elem': function() {
+				var list = this.mgr.list = [ new CKEDITOR.dom.element( 'div' ), new CKEDITOR.dom.element( 'span' ) ];
+				this.mgr.removeItem( list[ 1 ] );
+
+				assert.areSame( 1, this.mgr.list.length, 'Not all items were removed' );
+				assert.areSame( list[ 0 ], this.mgr.list[ 0 ], 'Correct element was removed' );
+			},
+
+			'test ViewerFocusManager.removeItem removes key listener': function() {
+				var removeListener = sinon.spy();
+				this.mgr.list = [ new CKEDITOR.dom.element( 'div' ) ];
+				this.mgr.list[ 0 ].removeListener = removeListener;
+
+				this.mgr.removeItem( 0 );
+
+				assert.areSame( 1, removeListener.callCount, 'CKEDITOR.dom.element.removeListener calls count' );
+				assert.areSame( 'keydown', removeListener.args[ 0 ][ 0 ], 'Removed listener type' );
+				assert.areSame( this.mgr.keyDownListener, removeListener.args[ 0 ][ 1 ], 'Listener function' );
+			},
+
+			'test elemnts are kept in DOM order': function() {
+				// If we'll add #input3 and #input2 the input #input2 should be placed
+				// as first item, because it's a younger DOM child.
+				var youngerChild = CKEDITOR.document.getById( 'input2' );
+				this.mgr.addItem( CKEDITOR.document.getById( 'input3' ) );
+				this.mgr.addItem( youngerChild );
+
+				assert.areSame( youngerChild, this.mgr.getItem( 0 ), 'Younger DOM child is placed first' );
+			},
+
+			'test ViewerFocusManager.getNext': function() {
+				// In this case we'll do multiple calls in same case to reduce TC count.
+				var list = this.mgr.list = [ newElement( 'div' ), newElement( 'a' ) ];
+
+				assert.areSame( list[ 0 ], this.mgr.getNext(), 'getNext() no param - return value' );
+				assert.areSame( list[ 1 ], this.mgr.getNext( 0 ), 'getNext( 0 ) - return value' );
+				// Index edge case.
+				assert.areSame( list[ 0 ], this.mgr.getNext( 1 ), 'getNext( 1 ) - return value' );
+				// Out of index boundary.
+				assert.areSame( list[ 0 ], this.mgr.getNext( 2 ), 'getNext( 2 ) - return value' );
+			},
+
+			'test ViewerFocusManager.getNext skips hidden elem': function() {
+				// getNext() should not return invisible elements.
+				// The a element (index 1) will be invisible one.
+				var list = this.mgr.list = [ newElement( 'div' ), newElement( 'a', false ), newElement( 'b' ) ];
+
+				assert.areSame( list[ 2 ], this.mgr.getNext( 0 ) );
+			},
+
+			'test ViewerFocusManager.getNext only hidden elem': function() {
+				// This list contains only invisible elements, so undefined should be returned.
+				this.mgr.list = [ newElement( 'div', false ), newElement( 'a', false ) ];
+
+				assert.areSame( undefined, this.mgr.getNext( 0 ) );
+			},
+
+			'test ViewerFocusManager.getPrev': function() {
+				// In this case we'll do multiple calls in same case to reduce TC count.
+				var list = this.mgr.list = [ newElement( 'div' ), newElement( 'a' ) ];
+
+				assert.areSame( list[ 1 ], this.mgr.getPrev(), 'getPrev() no param - return value' );
+				assert.areSame( list[ 1 ], this.mgr.getPrev( 0 ), 'getPrev( 0 ) - return value' );
+				// Index edge case.
+				assert.areSame( list[ 0 ], this.mgr.getPrev( 1 ), 'getPrev( 1 ) - return value' );
+				// Out of index boundary.
+				assert.areSame( list[ 1 ], this.mgr.getPrev( 2 ), 'getPrev( 2 ) - return value' );
+			},
+
+			'test ViewerFocusManager.getPrev skips hidden elem': function() {
+				// getPrev() should not return invisible elements.
+				// The a element (index 1) will be invisible one.
+				var list = this.mgr.list = [ newElement( 'div' ), newElement( 'a', false ), newElement( 'b' ) ];
+
+				assert.areSame( list[ 0 ], this.mgr.getPrev( 2 ) );
+			},
+
+			'test ViewerFocusManager.getPrev only hidden elem': function() {
+				// This list contains only invisible elements, so undefined should be returned.
+				this.mgr.list = [ newElement( 'div', false ), newElement( 'a', false ) ];
+
+				assert.areSame( undefined, this.mgr.getPrev( 1 ) );
+			},
+
+			'test ViewerFocusManager._sort': function() {
+				var input2 = CKEDITOR.document.getById( 'input2' ),
+					input3 = CKEDITOR.document.getById( 'input3' ),
+					_sort = ViewerFocusManager.prototype._sort;
+
+				assert.areSame( -1, _sort( input2, input3 ), 'sort( input2, input3 )' );
+				assert.areSame( 1, _sort( input3, input2 ), 'sort( input3, input2 )' );
+			}
+		} );
+
+		// @param {Boolean} [fakeIsVisible=true] Overrides isVisible method to return true.
+		function newElement( tagName, fakeIsVisible ) {
+			if ( fakeIsVisible === undefined ) {
+				fakeIsVisible = true;
+			}
+
+			var ret =  new CKEDITOR.dom.element( tagName );
+			if ( fakeIsVisible ) {
+				ret.isVisible = function() { return true; };
+			}
+			return ret;
+		}
+
+	} );
+} )();
---
+++ ui/ViewerFocusManager.js
@@ -0,0 +1,211 @@
+define( function() {
+	/**
+	 * @class CKEDITOR.plugins.a11ychecker.ui.ViewerFocusManager
+	 * @constructor
+	 */
+	function ViewerFocusManager() {
+		/**
+		 * @property {CKEDITOR.dom.element[]} A list of focusable elements.
+		 */
+		this.list = [];
+	}
+
+	ViewerFocusManager.prototype = {};
+	ViewerFocusManager.prototype.constructor = ViewerFocusManager;
+
+	/**
+	 * @returns {Number} Number of elements in focus manager.
+	 */
+	ViewerFocusManager.prototype.count = function() {
+		return this.list.length;
+	};
+
+	/**
+	 * Adds element to the focusable list.
+	 *
+	 * @param {CKEDITOR.dom.element} element
+	 */
+	ViewerFocusManager.prototype.addItem = function( element ) {
+		element.on( 'keydown', this.keyDownListener, this );
+
+		this.list.push( element );
+
+		// This might be resource consuming, we should execute _sort as long
+		// as it returns 1 and move inserted element upwards the list.
+		// But now KISS.
+		this.sort();
+	};
+
+	/**
+	 * @param index {Number/CKEDITOR.dom.element} Index or element to be removed from focus list.
+	 */
+	ViewerFocusManager.prototype.removeItem = function( index ) {
+		if ( typeof index != 'number' ) {
+			index = CKEDITOR.tools.indexOf( this.list, index );
+		}
+
+		this.list[ index ].removeListener( 'keydown', this.keyDownListener );
+
+		this.list.splice( index, 1 );
+	};
+
+	/**
+	 * Returns element at given index.
+	 *
+	 * @returns {CKEDITOR.dom.element/undefined} Returns requrested element or `undefined` if
+	 * not found.
+	 */
+	ViewerFocusManager.prototype.getItem = function( index ) {
+		return this.list[ index ];
+	};
+
+	/**
+	 * @private
+	 */
+	ViewerFocusManager.prototype.keyDownListener = function( evt ) {
+		var index = CKEDITOR.tools.indexOf( this.list, evt.sender ),
+			key = evt.data.getKey(),
+			keystroke = evt.data.getKeystroke();
+
+		// Early return if:
+		// Event source elem is not in this.list.
+		// List contains only 1 element.
+		// Pressed key is not a Tab.
+		if ( index === -1 || this.list.length === 1 || key != 9 /* Tab */ ) {
+			return ;
+		}
+
+		var shiftPressed = keystroke & CKEDITOR.SHIFT,
+			nextItem = shiftPressed ? this.getPrev( index ) : this.getNext( index );
+
+		// If every focusable element is not visible it's possible that undefined will be returned.
+		if ( nextItem ) {
+			nextItem.focus();
+			evt.data.preventDefault( 1 );
+			evt.data.stopPropagation();
+		}
+	};
+
+	/**
+	 * Returns next focusable element in list.
+	 *
+	 * First element will be returned when end is reached.
+	 *
+	 *		// Assuming that focusMgr has 3 elements.
+	 *		focusMgr.getNext(); // Returns first item.
+	 *		focusMgr.getNext( 0 ); // Returns second item.
+	 *		focusMgr.getNext( 2 ); // Returns first item.
+	 *
+	 * @param {Number} [startIndex] If given, next item according to that index will be returned.
+	 * @return {CKEDITOR.dom.element\undefined} Next node, or `undefined` if list is empty.
+	 */
+	ViewerFocusManager.prototype.getNext = function( startIndex ) {
+		var pickedIndex = 0,
+			listSize = this.list.length,
+			ret;
+
+		if ( typeof startIndex == 'number' && ( startIndex + 1 ) < listSize ) {
+			pickedIndex = startIndex + 1;
+		}
+
+		ret = this.getItem( pickedIndex );
+
+		// Following loop will make sure that no visible element is returned.
+		var i,
+			nextIndex;
+		// We can use for loop, since in worst case scenario it will be listSize - 1 iterations.
+		for ( i = 1; i < listSize - 1 && !ret.isVisible(); i ++ ) {
+			nextIndex = pickedIndex + i;
+			if ( nextIndex >= listSize ) {
+				nextIndex -= listSize;
+			}
+
+			ret = this.getItem( nextIndex );
+		}
+
+		if ( i == listSize - 1 && !ret.isVisible() ) {
+			// Edge case, last iteration still might return invisible elemnt, check
+			// that.
+			return undefined;
+		}
+
+		return ret;
+	};
+
+	/**
+	 * Returns previous focusable element in list.
+	 *
+	 * Last element will be returned when called with `0` index.
+	 *
+	 * Works the same way as {@link #next}.
+	 *
+	 * @param {Number} [startIndex] If given, previous item according to that index will be returned.
+	 * @return {CKEDITOR.dom.element\undefined} Previous node, or `undefined` if list is empty.
+	 */
+	ViewerFocusManager.prototype.getPrev = function( startIndex ) {
+		var listSize = this.list.length,
+			pickedIndex = listSize - 1,
+			ret;
+
+		if ( typeof startIndex == 'number' && startIndex > 0 ) {
+			pickedIndex = startIndex - 1;
+		}
+
+		ret = this.getItem( pickedIndex );
+
+		// Following loop will make sure that no visible element is returned.
+		// Basically reverted logic from getNext().
+		var i,
+			nextIndex;
+		// We can use for loop, since in worst case scenario it will be listSize - 1 iterations.
+		for ( i = 1; i < listSize - 1 && !ret.isVisible(); i ++ ) {
+			nextIndex = pickedIndex - i;
+			if ( nextIndex < 0 ) {
+				nextIndex = listSize + nextIndex;
+			}
+
+			ret = this.getItem( nextIndex );
+		}
+
+		if ( i == listSize - 1 && !ret.isVisible() ) {
+			// Edge case, last iteration still might return invisible elemnt, check
+			// that.
+			return undefined;
+		}
+
+		return ret;
+	};
+
+	/**
+	 * Sorts elements contained in {@link #list} array.
+	 */
+	ViewerFocusManager.prototype.sort = function() {
+		this.list.sort( this._sort );
+	};
+
+	/**
+	 * Sorting algorithm used in {@link #sort} method.
+	 *
+	 * It sorts list elements by their DOM position.
+	 *
+	 * @private
+	 * @param {CKEDITOR.dom.element} a First sort argument.
+	 * @param {CKEDITOR.dom.element} b Second sort argument.
+	 * @returns {Number}
+	 */
+	ViewerFocusManager.prototype._sort = function( a, b ) {
+		var ret = 0;
+
+		// If a is following b in DOM tree, then we mark b as earlier.
+		if ( a.getPosition( b ) & CKEDITOR.POSITION_FOLLOWING ) {
+			ret = 1;
+		} else {
+			ret = -1;
+		}
+
+		return ret;
+	};
+
+
+	return ViewerFocusManager;
+} );
