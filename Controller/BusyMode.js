64762470484646d47b6a8475e8164ee9a5a3cafd
Marek Lewandowski <m.lewandowski@cksource.com>
2014-09-29 15:50:39 +0200

    Introduced Busy Mode - it will notify end user that Accessibility Checker is loading results.

---
+++ Controller/BusyMode.js
@@ -0,0 +1,40 @@
+
+define( function() {
+
+	/**
+	 * A mode indicating that Accessibility Checker is busy. Used mainly to indicate
+	 * that issue searching is in progress, and engine has not returned any results yet.
+	 *
+	 * @class CKEDITOR.plugins.a11ychecker.BusyMode
+	 * @constructor
+	 */
+	function BusyMode( controller ) {
+		/**
+		 * @property {CKEDITOR.plugins.a11ychecker.Controller} controller
+		 */
+		this.controller = controller;
+	}
+
+	BusyMode.prototype = {};
+	BusyMode.prototype.constructor = BusyMode;
+
+	/**
+	 * Method to be called when controller enters this mode.
+	 *
+	 * @member CKEDITOR.plugins.a11ychecker.BusyMode
+	 */
+	BusyMode.prototype.init = function() {
+		this.controller.editor.editable().addClass( 'cke_loading' );
+	};
+
+	/**
+	 * Method to be called when controller leaves this mode.
+	 *
+	 * @member CKEDITOR.plugins.a11ychecker.BusyMode
+	 */
+	BusyMode.prototype.close = function() {
+		this.controller.editor.editable().removeClass( 'cke_loading' );
+	};
+
+	return BusyMode;
+} );
