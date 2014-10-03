d3f04b7029ccb208ee7c8cb690701cf17b13e61e
Marek Lewandowski <m.lewandowski@cksource.com>
2014-10-03 17:25:15 +0200

    Added tests for uninited a11ychecker next and prev commands.

--- tests/plugins/a11ychecker/Controller.integration.js
+++ tests/plugins/a11ychecker/Controller.integration.js
@@ -16,12 +16,29 @@
 		startupData: '<p>foo</p>'
 	};
 
-	require( [ 'Controller' ], function( Controller ) {
+	require( [ 'Controller', 'ui/ViewerController', 'EngineMock' ], function( Controller ) {
 		bender.test( {
 			'test non inited plugin .close()': function() {
 				// When plugin is not inited its .close() call shouldn't throw any
 				// exception.
 				this.editor._.a11ychecker.close();
+				// No exceptions, all fine.
+				assert.isTrue( true );
+			},
+
+			'test non inited plugin .next()': function() {
+				// When plugin is not inited its .close() call shouldn't throw any
+				// exception.
+				this.editor.execCommand( 'a11ychecker.next' );
+				// No exceptions, all fine.
+				assert.isTrue( true );
+			},
+
+			'test non inited plugin .prev()': function() {
+				// When plugin is not inited its .close() call shouldn't throw any
+				// exception.
+				//this.editor.execCommand( 'a11ychecker.prev' );
+				this.editor.execCommand( 'a11ychecker.prev' );
 				// No exceptions, all fine.
 				assert.isTrue( true );
 			}
