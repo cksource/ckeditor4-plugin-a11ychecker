113db5d7b3e8fe0e2b36f1e5bac2f1f25468deae
Marek Lewandowski <m.lewandowski@cksource.com>
2014-10-03 17:53:12 +0200

    Fixed #14 - Changing editor mode will close Accessibility checker.

--- tests/plugins/a11ychecker/Controller.integration.js
+++ tests/plugins/a11ychecker/Controller.integration.js
@@ -1,5 +1,5 @@
 /* bender-tags: editor,unit */
-/* bender-ckeditor-plugins: a11ychecker */
+/* bender-ckeditor-plugins: a11ychecker,sourcearea */
 /* bender-include: %TEST_DIR%_helpers/require.js, %TEST_DIR%_helpers/requireConfig.js */
 
 /**
@@ -16,7 +16,7 @@
 		startupData: '<p>foo</p>'
 	};
 
-	require( [ 'Controller', 'ui/ViewerController', 'EngineMock' ], function( Controller ) {
+	require( [ 'helpers/sinon/sinon_amd.min', 'Controller', 'ui/ViewerController', 'EngineMock' ], function( sinon, Controller ) {
 		bender.test( {
 			'test non inited plugin .close()': function() {
 				// When plugin is not inited its .close() call shouldn't throw any
@@ -27,7 +27,7 @@
 			},
 
 			'test non inited plugin .next()': function() {
-				// When plugin is not inited its .close() call shouldn't throw any
+				// When plugin is not inited its .next() call shouldn't throw any
 				// exception.
 				this.editor.execCommand( 'a11ychecker.next' );
 				// No exceptions, all fine.
@@ -35,12 +35,21 @@
 			},
 
 			'test non inited plugin .prev()': function() {
-				// When plugin is not inited its .close() call shouldn't throw any
+				// When plugin is not inited its .prev() call shouldn't throw any
 				// exception.
-				//this.editor.execCommand( 'a11ychecker.prev' );
 				this.editor.execCommand( 'a11ychecker.prev' );
 				// No exceptions, all fine.
 				assert.isTrue( true );
+			},
+
+			'test change to sourcemode': function() {
+				var a11ychecker = this.editor._.a11ychecker;
+
+				a11ychecker.close = sinon.spy( a11ychecker.close );
+
+				this.editor.execCommand( 'source' );
+
+				assert.areEqual( 1, a11ychecker.close.callCount, 'a11ychecker.close call count' );
 			}
 		} );
 	} );
