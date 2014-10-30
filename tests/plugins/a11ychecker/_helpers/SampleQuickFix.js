67a0d844e95ea86a6f43857ffe4f42ca44e765d0
Marek Lewandowski <m.lewandowski@cksource.com>
2014-10-30 17:07:15 +0100

    First attempt to implement QuickFix.Repository class.

---
+++ tests/plugins/a11ychecker/_helpers/SampleQuickFix.js
@@ -0,0 +1,17 @@
+(function() {
+	/**
+	 * @constructor
+	 */
+	function SampleQuickFix() {
+	}
+
+	SampleQuickFix.prototype = {};
+	SampleQuickFix.prototype.constructor = SampleQuickFix;
+
+	SampleQuickFix.prototype.foo = function() {
+		return 'bar';
+	};
+
+
+	CKEDITOR.plugins.a11ychecker.quickFixRepo.register( 'SampleQuickFix', SampleQuickFix );
+}());
