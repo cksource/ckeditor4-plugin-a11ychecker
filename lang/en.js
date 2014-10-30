2f6bcceece6677c33aabd54899db84646812ab5f
fredck <f.knabben@cksource.com>
2014-10-30 13:31:33 +0100

    Language file cleanup and minor edits.

--- lang/en.js
+++ lang/en.js
@@ -3,28 +3,24 @@
  * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 CKEDITOR.plugins.setLang( 'a11ychecker', 'en', {
-	toolbar: 'Do a11y check!',
-	title: 'A11y checker',
-	nextBtn: 'Next',
-	prevBtn: 'Prev',
-	refreshBtn: 'Refresh',
+	toolbar: 'Check Accessibility',
 	closeBtn: 'Close',
 	testability: {
 		'0': 'notice',
 		'0.5': 'warning',
 		'1': 'error'
 	},
-	ignoreBtnTitle: 'Ignore the issue',
 	ignoreBtn: 'Ignore',
+	ignoreBtnTitle: 'Ignore this issue',
 	stopIgnoreBtn: 'Stop ignoring',
-	listeningInfo: 'Waiting for manual changes of the content. When done, click <strong>Check again</strong> below.',
+	listeningInfo: 'Waiting for manual content changes. When done, click <strong>Check again</strong> below.',
 	listeningCheckAgain: 'Check again',
-	balloonLabel: 'Accessibility checker',
+	balloonLabel: 'Accessibility Checker',
 	navigationNext: 'Next',
-	navigationNextTitle: 'Next',
+	navigationNextTitle: 'Next issue',
 	navigationPrev: 'Previous',
-	navigationPrevTitle: 'Previous',
+	navigationPrevTitle: 'Previous issue',
 	quickFixButton: 'Quick fix',
-	quickFixButtonTitle: 'Quick fix',
+	quickFixButtonTitle: 'Quick fix this issue',
 	navigationCounter: 'Issue {current} of {total}'
 } );
