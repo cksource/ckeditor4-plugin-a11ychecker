454e38bd37b748c781b9e5760853bbfb8ff92212
Marek Lewandowski <m.lewandowski@cksource.com>
2014-11-03 11:24:13 +0100

    Added AttributeRename QuickFix.

---
+++ QuickFix/AttributeRename.js
@@ -0,0 +1,71 @@
+
+( function() {
+	'use strict';
+
+	CKEDITOR.plugins.a11ychecker.quickFixes.get( 'Manual', function( Manual ) {
+		/**
+		 * QuickFix renaming an attribute {@link #attributeName} to another name
+		 * {@link #attributeTargetName}.
+		 *
+		 * @member CKEDITOR.plugins.a11ychecker.quickfix
+		 * @class AttributeRename
+		 * @constructor
+		 * @param {CKEDITOR.plugins.a11ychecker.Issue} issue Issue QuickFix is created for.
+		 */
+		function AttributeRename( issue ) {
+			Manual.call( this, issue );
+		}
+
+		AttributeRename.prototype = new Manual();
+
+		AttributeRename.prototype.constructor = AttributeRename;
+
+		/**
+		 * Name of the attribute to be renamed.
+		 *
+		 * @member CKEDITOR.plugins.a11ychecker.quickfix.AttributeRename
+		 */
+		AttributeRename.prototype.attributeName = 'title';
+
+		/**
+		 * A desired name for the attribute.
+		 *
+		 * @member CKEDITOR.plugins.a11ychecker.quickfix.AttributeRename
+		 */
+		AttributeRename.prototype.attributeTargetName = 'alt';
+
+		AttributeRename.prototype.display = function( form ) {
+			var proposedValue = this.issue.element.getAttribute( this.attributeName) || '';
+
+			form.setInputs( {
+				value: {
+					type: 'text',
+					label: 'Value',
+					value: proposedValue
+				}
+			} );
+		};
+
+		/**
+		 * @param {Object} formAttributes Object containing serialized form inputs. See
+		 * {@link CKEDITOR.plugins.a11ychecker.ViewerForm#serialize}.
+		 * @param {Function} callback Function to be called when a fix was applied. Gets QuickFix object
+		 * as a first parameter.
+		 */
+		AttributeRename.prototype.fix = function( formAttributes, callback ) {
+			var issueElement = this.issue.element,
+				desiredValue = formAttributes.value;
+
+			// Set desired attribute value.
+			issueElement.setAttribute( this.attributeTargetName, desiredValue );
+			// Unset the old attribute.
+			issueElement.removeAttribute( this.attributeName );
+
+			if ( callback ) {
+				callback( this );
+			}
+		};
+
+		CKEDITOR.plugins.a11ychecker.quickFixes.add( 'AttributeRename', AttributeRename );
+	} );
+}() );
