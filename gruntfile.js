b90a50d81f5b52283dd31cad6e01699609590363
Marek Lewandowski <m.lewandowski@cksource.com>
2014-11-04 15:44:26 +0100

    Excluded external plugins READMEs.

--- gruntfile.js
+++ gruntfile.js
@@ -94,7 +94,7 @@ module.exports = function( grunt ) {
 
 			// Copies external dependencies into a build directory.
 			external: {
-				src: [ '../balloonpanel/**', '../a11ycheckerquail/**', '!../a11ycheckerquail/tests/**', '!../balloonpanel/tests/**' ],
+				src: [ '../balloonpanel/**', '../a11ycheckerquail/**', '!../a11ycheckerquail/tests/**', '!../a11ycheckerquail/README.md', '!../balloonpanel/tests/**', '!../balloonpanel/README.md' ],
 				dest: 'build/balloonpanel/'
 			},
 
