b944339ffe1400e5351beca85afdf034b11123a8
Marek Lewandowski <m.lewandowski@cksource.com>
2014-11-06 15:29:39 +0100

    Fixed #74 - Used load-grunt-tasks module to reduce grunt.loadNpmTasks call count.

--- gruntfile.js
+++ gruntfile.js
@@ -9,6 +9,9 @@
 'use strict';
 
 module.exports = function( grunt ) {
+
+	require( 'load-grunt-tasks' )( grunt );
+
 	grunt.initConfig( {
 		pkg: grunt.file.readJSON( 'package.json' ),
 
@@ -160,23 +163,10 @@ module.exports = function( grunt ) {
 		}
 	} );
 
-	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
-	grunt.loadNpmTasks( 'grunt-jscs' );
-	grunt.loadNpmTasks( 'grunt-githooks' );
-	grunt.loadNpmTasks( 'grunt-contrib-less' );
-	grunt.loadNpmTasks( 'grunt-contrib-watch' );
-	grunt.loadNpmTasks( 'grunt-contrib-copy' );
-	grunt.loadNpmTasks( 'grunt-contrib-clean' );
-	grunt.loadNpmTasks( 'grunt-contrib-uglify' );
-	grunt.loadNpmTasks( 'grunt-contrib-compress' );
-	grunt.loadNpmTasks( 'grunt-env' );
-	grunt.loadNpmTasks( 'grunt-preprocess' );
-
 	grunt.registerTask( 'build-css', 'Builds production-ready CSS using less.',
 		[ 'less:development', 'less:production' ] );
 	grunt.registerTask( 'build-js', 'Build JS files.', buildJs );
 
-	//grunt.registerTask( 'plugin-versions', 'Replaces %REV% and %VERSION% strings in plugin.js.', markPluginVersions );
 	grunt.registerMultiTask( 'plugin-versions', 'Replaces %REV% and %VERSION% strings in plugin.js.', markPluginVersions );
 	grunt.registerTask( 'process', 'Process the HTML files, removing some conditional markup, ' +
 		'and replaces revsion hashes.', [ 'env:build', 'preprocess:build', 'plugin-versions' ] );
