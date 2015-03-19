ecec05198e8f3bd62fa13c3b1a314b4832f04542
Marek Lewandowski <m.lewandowski@cksource.com>
2015-03-19 15:33:03 +0100

    Updated Quail to cksource/quail at a1ce206.

--- libs/quail/quail.jquery.js
+++ libs/quail/quail.jquery.js
@@ -5525,7 +5525,6 @@ quail.pNotUsedAsHeader = function(quail, test, Case) {
 };
 
 quail.pNotUsedAsHeader = function(quail, test, Case) {
-  var priorStyle = { };
   test.get('$scope').find('p').each(function() {
     var _case = Case({
       element : this,
@@ -5539,7 +5538,9 @@ quail.pNotUsedAsHeader = function(quail, test, Case) {
     }
     var failed = false;
     if ($(this).text().search('.') < 1) {
-      var $paragraph = $(this);
+      var $paragraph = $(this),
+        priorParagraph = $paragraph.prev('p');
+      // Checking if any of suspectPHeaderTags has exact the same text as a paragraph.
       $.each(quail.suspectPHeaderTags, function(index, tag) {
         if ($paragraph.find(tag).length) {
           $paragraph.find(tag).each(function() {
@@ -5552,16 +5553,19 @@ quail.pNotUsedAsHeader = function(quail, test, Case) {
           });
         }
       });
-      $.each(quail.suspectPCSSStyles, function(index, style) {
-        if (typeof priorStyle[style] !== 'undefined' &&
-           priorStyle[style] !== $paragraph.css(style)) {
-          _case.set({
-            'status': 'failed'
-          });
-          failed = true;
-        }
-        priorStyle[style] = $paragraph.css(style);
-      });
+
+      // Checking if previous paragraph has a different values for style properties given in quail.suspectPCSSStyles.
+      if ( priorParagraph.length ) {
+        $.each(quail.suspectPCSSStyles, function(index, cssProperty) {
+          if ( $paragraph.css(cssProperty) !== priorParagraph.css(cssProperty) ) {
+            _case.set({
+              'status': 'failed'
+            });
+            failed = true;
+            return false; // Micro optimization - we no longer need to iterate here. jQuery css() method might be expansive.
+          }
+        });
+      }
       if ($paragraph.css('font-weight') === 'bold') {
         _case.set({
           'status': 'failed'
