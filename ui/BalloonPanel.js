494352e9d1d0e45b4fb3e87858ccf861c95d723d
Marek Lewandowski <m.lewandowski@cksource.com>
2015-04-14 15:22:24 +0200

    Simplified code.

--- ui/BalloonPanel.js
+++ ui/BalloonPanel.js
@@ -1,5 +1,5 @@
 /**
- * @license Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
+ * @license Copyright (c) 2003-2015, CKSource - Frederico KnascrollOffseten. All rights reserved.
  * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
@@ -20,34 +20,31 @@ define( function() {
 	BalloonPanel.prototype._isElementInViewport = function( element, window ) {
 		var scrollPos = window.getScrollPosition(),
 			elemRect = element.getClientRect(),
-			viewportSize = window.getViewPaneSize();
-
-		// Storing rects as 2 points.
-		var elemOffset = {
-			x1: element.$.offsetLeft,
-			y1: element.$.offsetTop,
-			x2: element.$.offsetLeft + elemRect.width,
-			y2: element.$.offsetTop + elemRect.height
-		};
-
-		var scrollOffset = {
-			x1: scrollPos.x,
-			y1: scrollPos.y,
-			x2: scrollPos.x + viewportSize.width,
-			y2: scrollPos.y + viewportSize.height
-		};
-
-		var AA = elemOffset,
-			BB = scrollOffset;
+			viewportSize = window.getViewPaneSize(),
+			// Storing rects as 2 points.
+			elemOffset = {
+				x1: element.$.offsetLeft,
+				y1: element.$.offsetTop,
+				x2: element.$.offsetLeft + elemRect.width,
+				y2: element.$.offsetTop + elemRect.height
+			},
+			scrollOffset = {
+				x1: scrollPos.x,
+				y1: scrollPos.y,
+				x2: scrollPos.x + viewportSize.width,
+				y2: scrollPos.y + viewportSize.height
+			},
+			// I know I might initialize conds here, but it's actually app logic, and it wouldn't
+			// differentiate well enough from the rest of the code.
+			conds;
 
 		// We test this case by a negation.
 		// If one rect would be within another it wouldn't match to any of these conditions.
-		var conds = [
-			AA.x1 > BB.x2,
-			AA.x2 < BB.x1,
-			//false, //,
-			AA.y1 > BB.y2,
-			AA.y2 < BB.y1
+		conds = [
+			elemOffset.x1 > scrollOffset.x2,
+			elemOffset.x2 < scrollOffset.x1,
+			elemOffset.y1 > scrollOffset.y2,
+			elemOffset.y2 < scrollOffset.y1
 		];
 
 		// If none of the conditions is true, then it's not overlapping.
