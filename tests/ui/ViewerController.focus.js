bd1fcfb42dd2c4b2ce268897612e094fdf2dd1d4
m.lewandowski <m.lewandowski@cksource.com>
2016-03-17 08:26:35 +0100

    Work in progress: adapting tests to latest CKEditor test environment changes - bender.tools.setUpEditors was removed.

--- tests/ui/ViewerController.focus.js
+++ tests/ui/ViewerController.focus.js
@@ -8,122 +8,136 @@
 
 ( function() {
 	'use strict';
-
+	
 	// Note that we have an extra (unused) requirement for 'EngineMock' and 'Controller' classes.
 	// That way it will force them to be available for the editor, and we have sure that a11ychecker
 	// plugin will be ready synchronously.
 	bender.require( [ 'testSuite', 'EngineMock' ], function( testSuite, EngineMock ) {
+		
+		bender.editors = {
+			classic: {
+				name: 'editor1',
+				config: {
+					on: {}
+				},
+				startupData: '<p>foo</p>'
+			},
+			inline: {
+				name: 'editor2',
+				creator: 'inline',
+				config: {
+					on: {}
+				},
+				startupData: '<p>foo</p>'
+			}
+		};
+		
+		var loader = {
+			loaded: 0,
+			editorsCount: CKEDITOR.tools.objectKeys( bender.editors ).length,
+			editorLoaded: function( evt ) {
+				// @todo: init engine
+				console.log('called', evt, evt && evt.editor);
+				evt.editor._.a11ychecker.engine = new EngineMock();
+				
+				if ( !loader.isDone() ) {
+					loader.loaded += 1;
+				}
+				
+				if ( loader.isDone() && loader.done ) {
+					loader.done();
+				}
+			},
+			done: null,
+			isDone: function() {
+				return this.loaded >= this.editorsCount;
+			}
+		};
 
-		bender.test( {
+		var tests = {
 			'async:init': function() {
 				var that = this;
-
-				bender.tools.setUpEditors( {
-					classic: {
-						name: 'editor1',
-						config: {},
-						startupData: '<p>foo</p>'
-					},
-					inline: {
-						name: 'editor2',
-						creator: 'inline',
-						config: {},
-						startupData: '<p>foo</p>'
+				
+				loader.done = function() {
+					console.log('loader is done');
+					that.callback();
+				};
+				
+				for ( var editorName in this.editors ) {
+					var editor = this.editors[ editorName ],
+						a11ychecker = editor._.a11ychecker;
+					
+					if ( a11ychecker.exec ) {
+						// If by any chance it's already real object, run synchronously.
+						// IE tends to laod it synchronously.
+						loader.editorLoaded( { editor: this.editors[ editorName ] } );
+					} else {
+						// Otherwise listen for event.
+						a11ychecker.once( 'loaded', loader.editorLoaded, editor );
 					}
-				}, function( editors, bots ) {
-					that.editorBots = bots;
-					that.editors = editors;
-					that.editor = editors.classic;
-
-					// Make sure that we run tests when AC for both instances is loaded.
-					var loaded = 0,
-						editorsCount = 2,
-						loadedCallback = function( evt ) {
-							loaded += 1;
-
-							this._.a11ychecker.engine = new EngineMock();
-
-							if ( loaded === editorsCount ) {
-								that.callback();
-							}
-						},
-						listenForLoad = function( editor ) {
-							var a11ychecker = editor._.a11ychecker;
-							if ( a11ychecker.exec ) {
-								// If by any chance it's already real object, run synchronously.
-								// IE tends to laod it synchronously.
-								loadedCallback.call( editor );
-							} else {
-								// Otherwise listen for event.
-								a11ychecker.once( 'loaded', loadedCallback, editor );
-							}
-						};
-
-					// Adding listener for both instances.
-					listenForLoad( editors.inline );
-					listenForLoad( editors.classic );
-				} );
+				}
 			},
 
 			tearDown: function() {
 				// For each test a11ychecker needs to be closed.
 				// Note that we have 2 editor instances, but only 1 can be enabled at
 				// a time.
-				this.editor._.a11ychecker.close();
+				//console.log( 'is it there?', this.editors.classic._.a11ychecker.close );
+				this.editors.classic._.a11ychecker.close();
 				this.editors.inline._.a11ychecker.close();
 			},
 
-			'test initial focus': function() {
-				var a11ychecker = this.editor._.a11ychecker;
+			'test initial focus': function( editor ) {
+				var a11ychecker = editor._.a11ychecker;
 				a11ychecker.exec();
 				a11ychecker.showIssue( 0 );
 				assert.isTrue( true );
 			},
-
-			'test next focus': function() {
-				var a11ychecker = this.editor._.a11ychecker,
+			
+			'test next focus': function( editor ) {
+				var a11ychecker = editor._.a11ychecker,
 					viewer = a11ychecker.viewerController.viewer;
 				a11ychecker.exec();
 				a11ychecker.showIssue( 0 );
 				a11ychecker.next( function() {
 					resume( function() {
 						var activeElement = CKEDITOR.document.getActive();
-
+			
 						assert.isNotNull( activeElement, 'An element is focused' );
 						assert.areSame( viewer.navigation.parts.next, activeElement, 'The "Next" button is focused' );
 					} );
 				} );
-
+			
 				wait();
 			},
-
-			'test prev focus': function() {
-				var a11ychecker = this.editor._.a11ychecker,
+			
+			'test prev focus': function( editor ) {
+				var a11ychecker = editor._.a11ychecker,
 					viewer = a11ychecker.viewerController.viewer;
 				a11ychecker.exec();
 				a11ychecker.showIssue( 0 );
 				a11ychecker.prev( function() {
 					resume( function() {
 						var activeElement = CKEDITOR.document.getActive();
-
+			
 						assert.isNotNull( activeElement, 'An element is focused' );
 						assert.areSame( viewer.navigation.parts.previous, activeElement, 'The "Previous" button is focused' );
 					} );
 				} );
-
+			
 				wait();
 			},
-
-			'test focus on click': function() {
-				var a11ychecker = this.editor._.a11ychecker,
+			
+			'test focus on click': function( editor ) {
+				var a11ychecker = editor._.a11ychecker,
 					viewer = a11ychecker.viewerController.viewer,
 					expectedFocusElem = viewer.navigation.parts.next;
-
+			
 				// Perform check, so we have a11ychecker.list
 				a11ychecker.check( {
 					ui: true
 				} );
-
+			
 				var issuedElement = a11ychecker.issues.getItem( 0 ).element,
 					evt = {
 						data: {
@@ -134,77 +148,82 @@
 					};
 				// Ensure that element is not marked as focused.
 				issuedElement.removeClass( 'cke_a11yc_focused' );
-
+			
 				a11ychecker.editableDecorator.clickListener( evt );
-
+			
 				// Remember that balloon shows with a delay, so we need to workaround that.
 				window.setTimeout( function() {
 					resume( function() {
 						var activeElement = CKEDITOR.document.getActive();
-
+			
 						assert.isNotNull( activeElement, 'An element is focused' );
 						assert.areSame( expectedFocusElem, activeElement, 'The "Next" button is focused' );
 					} );
 				}, 500 );
-
+			
 				wait();
 			},
-
-			'test focus trap': function() {
+			
+			'test focus trap': function( editor ) {
 				// Dialog should have focus trap applied.
 				// That means that tab press at the last focusable element should take you to the
 				// first one and vice versa.
-				var a11ychecker = this.editor._.a11ychecker,
+				var a11ychecker = editor._.a11ychecker,
 					viewer = a11ychecker.viewerController.viewer,
-					initialFocusElem = this._getLastFocusable( viewer ),
+					initialFocusElem = getLastFocusable( viewer ),
 					expectedFocusElem = viewer.navigation.parts.previous;
 				a11ychecker.exec();
 				// Will focus the last button.
 				initialFocusElem.focus();
-
+			
 				window.setTimeout( function() {
 					resume( function() {
 						initialFocusElem.fire( 'keydown', getKeyEvent( 9 ) );
-
+			
 						var activeElement = CKEDITOR.document.getActive();
 						assert.areSame( expectedFocusElem, activeElement, 'Invalid element focused' );
 					} );
 				}, 300 );
-
+			
 				wait();
 			},
-
-			'test focus trap reversed': function() {
+			
+			'test focus trap reversed': function( editor ) {
 				// Dialog should have focus trap applied.
 				// That means that shift-tab at first focusable element should take you to the last one,
 				// and vice versa.
-				var a11ychecker = this.editor._.a11ychecker,
+				var a11ychecker = editor._.a11ychecker,
 					viewer = a11ychecker.viewerController.viewer,
-					expectedFocusElem = this._getLastFocusable( viewer );
+					expectedFocusElem = getLastFocusable( viewer );
 				a11ychecker.exec();
 				// Will focus prev button.
 				a11ychecker.prev();
-
+			
 				window.setTimeout( function() {
 					resume( function() {
 						viewer.navigation.parts.previous.fire( 'keydown', getKeyEvent( CKEDITOR.SHIFT + 9 ) );
-
+			
 						var activeElement = CKEDITOR.document.getActive();
 						assert.areSame( expectedFocusElem, activeElement, 'Invalid element focused' );
 					} );
 				}, 300 );
-
+			
 				wait();
 			},
-
-			'test inline editor focus with balloon': function() {
+			
+			'test inline editor focus with balloon': function( editor ) {
+				if ( editor.name != 'inline' ) {
+					//bender.ignore();
+					assert.isTrue( true );
+					return ;
+				}
+				
 				// This test will ensure that after showing the balloon (.next() method) editor
 				// stays marked as focused, therefore it won't be blured.
-				var editor = this.editors.inline,
-					a11ychecker = editor._.a11ychecker;
-
+				var a11ychecker = editor._.a11ychecker;
+			
 				a11ychecker.exec();
-
+			
 				a11ychecker.next( function() {
 					// Put an extra timeout.
 					window.setTimeout( function() {
@@ -213,32 +232,32 @@
 						} );
 					}, 500 );
 				} );
-
+			
 				wait();
 			},
-
-			'test a11ychecker.exec focus': function() {
+			
+			'test a11ychecker.exec focus': function( editor ) {
 				// For exec function the focus should go to the next button.
-				var a11ychecker = this.editor._.a11ychecker,
+				var a11ychecker = editor._.a11ychecker,
 					viewer = a11ychecker.viewerController.viewer,
 					expectedFocusElem = viewer.navigation.parts.next;
 				a11ychecker.exec();
-
+			
 				window.setTimeout( function() {
 					resume( function() {
 						var activeElement = CKEDITOR.document.getActive();
 						assert.areSame( expectedFocusElem, activeElement, 'Invalid element focused' );
 					} );
 				}, 300 );
-
+			
 				wait();
-			},
-
-			// Returns the last focusable element in viewer.
-			_getLastFocusable: function( viewer ) {
-				return viewer.form.parts.ignoreButton;
 			}
-		} );
+		};
+		
+		// Returns the last focusable element in viewer.
+		function getLastFocusable( viewer ) {
+			return viewer.form.parts.ignoreButton;
+		}
 
 		// Returns a key event mockup.
 		// @param {Number} keystroke Keystroke with modifiers. eg. CKEDITOR.SHIFT + 9 // shift + tab
@@ -259,5 +278,7 @@
 				}
 			};
 		}
+		
+		bender.test( bender.tools.createTestsForEditors( CKEDITOR.tools.objectKeys( bender.editors ), tests ) );
 	} );
 } )();
